c012ae6ec8d5cf2ce66cad262e9842d6106558b4
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-12-30 15:57:54 +0000

    Add mutations for adding and removing item, add actions for adding and removing item mutations

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,25 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    items: []
+    items: [],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    addItem(state, item) {
+      state.items.push(item);
+    },
+    removeItem(state, index) {
+      state.items = state.items.filter((item, i) => i !== index);
+    },
   },
   actions: {
+    addItem({ commit }, item) {
+      commit("addItem", item);
+    },
+    removeItem({ commit }, index) {
+      commit("removeItem", index);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
