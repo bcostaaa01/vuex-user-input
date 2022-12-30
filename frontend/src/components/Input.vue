4a44166eec7a512cd35074f77d52dc7ee23661ab
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-12-30 15:53:42 +0000

    Add input field, render items in UI, add remove item functionality

--- frontend/src/components/Input.vue
+++ frontend/src/components/Input.vue
@@ -1,19 +1,28 @@
 <template>
-    <div>
-        Testing component look
-    </div>
+  <div>
+    <form @prevent.default="addItem">
+      <input v-model="newItem" placeholder="Add a new item" />
+      <button type="submit">Add</button>
+    </form>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+        <button @click="removeItem(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import store from "../store/index"
+import store from "../store/index";
 
 export default {
-    name: "Input",
-    store,
-    data() {
-        return {
-            newItem: ''
-        }
-    }
-}
+  name: "Input",
+  store,
+  data() {
+    return {
+      newItem: "",
+    };
+  },
+};
 </script>
